a3706677b7052f1b286431e2d09519a32582f697
Mamadou Samba DIALLO <sdiallo@MacBook-Pro-de-ENEANCE-2.local>
2023-04-21 12:03:18 +0200

    backend added

---
+++ shoes-store-backend/database/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+function DB() {
+  mongoose
+    .connect(process.env.URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("connected to mongodb"))
+    .catch((e) => console.log(e));
+}
+
+module.exports = DB;
---
+++ shoes-store-backend/validations/signupValidation.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+function SignupValidation(obj) {
+  const schema = Joi.object({
+    fullname: Joi.string().min(5).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(5).required(),
+  });
+
+  const { error } = schema.validate(obj);
+  return error;
+}
+
+module.exports = SignupValidation;
